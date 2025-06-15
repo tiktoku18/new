a3c87f0c6544c6a579224ec9358475d82cd39d1c
tiktoku18 <issaselemanimgogo@gmail.com>
2025-06-15 23:30:25 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['i.pravatar.cc'],
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
